cad770ec00b259e310c3e49a359f12e66e3498c6
yellows8 <yellows8@users.noreply.github.com>
2024-01-29 14:19:02 -0500

    tegra: Improved usb handling.

--- hw/arm/tegra2/ahb/usb/usb.c
+++ hw/arm/tegra2/ahb/usb/usb.c
@@ -31,9 +31,17 @@
 typedef struct tegra_usb_state {
     EHCISysBusState parent_obj;
 
-    DEFINE_REG32(usb1_if_usb_susp_ctrl);
+    uint32_t regs_config[0x100>>2];
 
+    uint32_t regs_controller[(0x260-0x1B4)>>2];
+    DEFINE_REG32(usb1_if_usb_susp_ctrl);
+    uint32_t regs_susp[(0x85C-0x404)>>2];
+    uint32_t regs_queue[0x80>>2];
+
+    MemoryRegion iomem_config;
+    MemoryRegion iomem_controller;
     MemoryRegion iomem;
+    MemoryRegion iomem_queue;
 } tegra_usb;
 
 static const VMStateDescription vmstate_tegra_usb = {
@@ -43,9 +51,69 @@ static const VMStateDescription vmstate_tegra_usb = {
     .fields = (VMStateField[]) {
         VMSTATE_STRUCT(ehci, EHCISysBusState, 2, vmstate_ehci, EHCIState),
         VMSTATE_UINT32(usb1_if_usb_susp_ctrl.reg32, tegra_usb),
+        VMSTATE_UINT32_ARRAY(regs_config, tegra_usb, 0x100>>2),
+        VMSTATE_UINT32_ARRAY(regs_controller, tegra_usb, (0x260-0x1B4)>>2),
+        VMSTATE_UINT32_ARRAY(regs_susp, tegra_usb, (0x85C-0x404)>>2),
+        VMSTATE_UINT32_ARRAY(regs_queue, tegra_usb, 0x80>>2),
         VMSTATE_END_OF_LIST()
     }
 };
+
+static uint64_t tegra_usb_config_priv_read(void *opaque, hwaddr offset,
+                                           unsigned size)
+{
+    tegra_usb *s = opaque;
+    uint64_t ret = 0;
+
+    assert(offset+size <= sizeof(s->regs_config));
+
+    ret = s->regs_config[offset/sizeof(uint32_t)] & ((1ULL<<size*8)-1);
+
+    TRACE_READ(s->iomem_config.addr, offset, ret);
+
+    return ret;
+}
+
+static void tegra_usb_config_priv_write(void *opaque, hwaddr offset,
+                                        uint64_t value, unsigned size)
+{
+    tegra_usb *s = opaque;
+
+    assert(offset+size <= sizeof(s->regs_config));
+
+    TRACE_WRITE(s->iomem_config.addr, offset, 0, value);
+
+    if (offset >= 0x80) {
+        s->regs_config[offset/sizeof(uint32_t)] = (s->regs_config[offset/sizeof(uint32_t)] & ~((1ULL<<size*8)-1)) | value;
+    }
+}
+
+static uint64_t tegra_usb_controller_priv_read(void *opaque, hwaddr offset,
+                                          unsigned size)
+{
+    tegra_usb *s = opaque;
+    uint64_t ret = 0;
+
+    assert(offset+size <= sizeof(s->regs_controller));
+
+    ret = s->regs_controller[offset/sizeof(uint32_t)] & ((1ULL<<size*8)-1);
+
+    TRACE_READ(s->iomem_controller.addr, offset, ret);
+
+    return ret;
+}
+
+static void tegra_usb_controller_priv_write(void *opaque, hwaddr offset,
+                                       uint64_t value, unsigned size)
+{
+    tegra_usb *s = opaque;
+
+    assert(offset+size <= sizeof(s->regs_controller));
+
+    TRACE_WRITE(s->iomem_controller.addr, offset, 0, value);
+
+    s->regs_controller[offset/sizeof(uint32_t)] = (s->regs_controller[offset/sizeof(uint32_t)] & ~((1ULL<<size*8)-1)) | value;
+}
 
 static uint64_t tegra_usb_priv_read(void *opaque, hwaddr offset,
                                     unsigned size)
@@ -58,6 +126,10 @@ static uint64_t tegra_usb_priv_read(void *opaque, hwaddr offset,
         ret = s->usb1_if_usb_susp_ctrl.reg32;
         break;
     default:
+        if (offset+size <= sizeof(s->regs_susp)+0x4) {
+            offset-=0x4;
+            ret = s->regs_config[offset/sizeof(uint32_t)] & ((1ULL<<size*8)-1);
+        }
         break;
     }
 
@@ -92,8 +164,39 @@ static void tegra_usb_priv_write(void *opaque, hwaddr offset,
         break;
     default:
         TRACE_WRITE(s->iomem.addr, offset, 0, value);
+        if (offset+size <= sizeof(s->regs_susp)+0x4) {
+            offset-=0x4;
+            s->regs_susp[offset/sizeof(uint32_t)] = (s->regs_susp[offset/sizeof(uint32_t)] & ~((1ULL<<size*8)-1)) | value;
+        }
         break;
     }
+}
+
+static uint64_t tegra_usb_queue_priv_read(void *opaque, hwaddr offset,
+                                          unsigned size)
+{
+    tegra_usb *s = opaque;
+    uint64_t ret = 0;
+
+    assert(offset+size <= sizeof(s->regs_queue));
+
+    ret = s->regs_queue[offset/sizeof(uint32_t)] & ((1ULL<<size*8)-1);
+
+    TRACE_READ(s->iomem_queue.addr, offset, ret);
+
+    return ret;
+}
+
+static void tegra_usb_queue_priv_write(void *opaque, hwaddr offset,
+                                       uint64_t value, unsigned size)
+{
+    tegra_usb *s = opaque;
+
+    assert(offset+size <= sizeof(s->regs_queue));
+
+    TRACE_WRITE(s->iomem_queue.addr, offset, 0, value);
+
+    s->regs_queue[offset/sizeof(uint32_t)] = (s->regs_queue[offset/sizeof(uint32_t)] & ~((1ULL<<size*8)-1)) | value;
 }
 
 static void tegra_usb_priv_reset(DeviceState *dev)
@@ -104,12 +207,35 @@ static void tegra_usb_priv_reset(DeviceState *dev)
 
     s->usb1_if_usb_susp_ctrl.reg32 = USB1_IF_USB_SUSP_CTRL_RESET;
 
+    memset(s->regs_config, 0, sizeof(s->regs_config));
+    memset(s->regs_controller, 0, sizeof(s->regs_controller));
+    memset(s->regs_susp, 0, sizeof(s->regs_susp));
+    memset(s->regs_queue, 0, sizeof(s->regs_queue));
+
     ehci_reset(ehci);
 }
+
+static const MemoryRegionOps tegra_usb_config_mem_ops = {
+    .read = tegra_usb_config_priv_read,
+    .write = tegra_usb_config_priv_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static const MemoryRegionOps tegra_usb_controller_mem_ops = {
+    .read = tegra_usb_controller_priv_read,
+    .write = tegra_usb_controller_priv_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
 
 static const MemoryRegionOps tegra_usb_mem_ops = {
     .read = tegra_usb_priv_read,
     .write = tegra_usb_priv_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+static const MemoryRegionOps tegra_usb_queue_mem_ops = {
+    .read = tegra_usb_queue_priv_read,
+    .write = tegra_usb_queue_priv_write,
     .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
@@ -119,29 +245,38 @@ static void tegra_usb_priv_init(Object *obj)
     tegra_usb *s = TEGRA_USB(obj);
     EHCIState *ehci = &i->ehci;
 
+    if (tegra_board >= TEGRAX1_BOARD) {
+        ehci->capsbase  = 0x100;
+        ehci->opregbase = 0x130;
+        ehci->portscbase = 0x174 - ehci->opregbase;
+        ehci->portnr = 1;
+    }
+    else {
+        ehci->capsbase  = 0x100;
+        ehci->opregbase = 0x140;
+    }
+
+    memory_region_init_io(&s->iomem_config, OBJECT(s), &tegra_usb_config_mem_ops, s,
+                          "tegra.usb_config", 0x100);
+    memory_region_init_io(&s->iomem_controller, OBJECT(s), &tegra_usb_controller_mem_ops, s,
+                          "tegra.usb_controller", 0x260-0x1B4);
     memory_region_init_io(&s->iomem, OBJECT(s), &tegra_usb_mem_ops, s,
-                          "tegra.usb_susp_ctrl", 4);
+                          "tegra.usb_susp_ctrl", 0x85C-0x400);
+    memory_region_init_io(&s->iomem_queue, OBJECT(s), &tegra_usb_queue_mem_ops, s,
+                          "tegra.usb_queue", 0x80);
+    memory_region_add_subregion(&ehci->mem, 0x0, &s->iomem_config);
+    memory_region_add_subregion(&ehci->mem, 0x1B4, &s->iomem_controller);
     memory_region_add_subregion(&ehci->mem, 0x400, &s->iomem);
+    //memory_region_add_subregion(&ehci->mem, 0x1000, &s->iomem_queue); // TODO: Fix this. ehci uses memory_region_init with size 0x1000, so memory_region_add_subregion can't be used with that here.
 }
 
 static void tegra_usb_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
-    SysBusEHCIClass *sec = SYS_BUS_EHCI_CLASS(klass);
 
     dc->vmsd = &vmstate_tegra_usb;
     dc->reset = tegra_usb_priv_reset;
 
-    if (tegra_board >= TEGRAX1_BOARD) {
-        sec->capsbase  = 0x100;
-        sec->opregbase = 0x130;
-        sec->portscbase = 0x174 - sec->opregbase;
-        sec->portnr = 1;
-    }
-    else {
-        sec->capsbase  = 0x100;
-        sec->opregbase = 0x140;
-    }
     set_bit(DEVICE_CATEGORY_USB, dc->categories);
 }
 
